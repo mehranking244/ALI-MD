b5dc868beb70673ee7cf95abe87a6acc860f1358
mehranking244 <nasirkingsana24@gmail.com>
2025-04-19 17:14:53 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "4LtUTDSC#xgPlFN45pXN3zEJU4VrG1DxkFmyysUjKHjF4JHc8leA",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
